9fd9bc60bf832a96da1003ae992596a5148b9b38
Dingyi Zhang <zhangdingyi2022@outlook.com>
2026-01-11 12:02:56 +0800

    新功能：显示窗口 新功能：存储日志 UI：RinUI

---
+++ src/app.py
@@ -0,0 +1,19 @@
+import sys
+from PySide6.QtWidgets import QApplication
+
+from RinUI import RinUIWindow
+
+from loguru import logger
+
+from core.playVideos import AppCentral
+
+logger.add("./logs/app.log", rotation="10 MB", retention="10 days", level="DEBUG")
+
+if __name__ == '__main__':
+    logger.info("App started.")
+    app = QApplication(sys.argv)
+    gallery = RinUIWindow("./qml/app.qml")
+    instance = AppCentral()
+    gallery.engine.rootContext().setContextProperty("AppCentral", instance)
+    app.exec()
+    logger.info("App ended.")
