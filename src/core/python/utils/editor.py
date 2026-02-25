838b047b15347197f9ecd3f7e4a574e2aefc18e0
Dingyi Zhang <zhangdingyi2022@outlook.com>
2026-02-25 22:17:16 +0800

    新功能：自动修改 settings.json

---
+++ src/core/python/utils/editor.py
@@ -0,0 +1,28 @@
+from json import load, dump
+
+from core.python.directories import CONFIG_PATH
+
+
+class ConfigEditor:
+    def __init__(self):
+        self.config = {}
+        self.config_path = CONFIG_PATH / "settings.json"
+
+    def _load(self):
+        with open(self.config_path, 'r', encoding='UTF-8') as f:
+            self.config = load(f)
+
+    def _edit(self, key, value):
+        self.config[key] = value
+
+    def _save(self):
+        with open(self.config_path, 'w', encoding='UTF-8') as f:
+            dump(self.config, f, ensure_ascii=False, indent=2)
+
+    def get(self, key):
+        return self.config[key]
+
+    def setconfig(self, key, value):
+        self._load()
+        self._edit(key, value)
+        self._save()
